6f644d276e48dcceede51fe3f740e2a0fac1945d
carlosreiakvam <cre032@uit,no>
2021-04-12 09:43:16 +0200

    eksplisitte spørringer

--- models/kommentar.py
+++ models/kommentar.py
@@ -3,33 +3,37 @@ from extensions import db
 from flask import abort
 
 
-# TODO: Test og reparer :). Legg merke til TODO lenger ned også
 class Kommentar:
     def __init__(self,
                  id: int = None,
                  innhold: int = None,
-                 brukernavn: str = None):
+                 brukernavn: str = None,
+                 dato: str = None):
         self.id = id
         self.innhold = innhold
         self.brukernavn = brukernavn
+        self.dato = dato
 
     @staticmethod
-    def get_all() -> List["Kommentar"]:
+    def get_all(innlegg_id: int) -> List["Kommentar"]:
         query = """
-        select *
-        from kommentarer 
-        order by dato
+        select 
+            kommentar_id, kommentar_innhold, kommentar_dato, bruker_navn
+        from kommentarer
+        where innlegg_id = %s
+        order by kommentar_dato
         """
-        db.cursor.execute(query)
+        db.cursor.execute(query, innlegg_id, )
         result = [Kommentar(*x) for x in db.cursor.fetchall()]
         return result
 
     @staticmethod
     def get_kommentar(kommentar_id: int) -> "Kommentar":
         query = """
-        select *
+        select 
+            kommentar_id, kommentar_innhold, kommentar_dato, bruker_navn
         from kommentar 
-        where id = %s
+        where kommentar_id = %s
         """
         db.cursor.execute(query, (kommentar_id,))
         result = Kommentar(*db.cursor.fetchone())
@@ -38,24 +42,18 @@ class Kommentar:
         else:
             abort(404)
 
-    def insert_kommentar(self) -> "Kommentar":
-        # TODO: Få inn innlegg_id fra innlegg model
+    def insert_kommentar(self, innlegg_id) -> "Kommentar":
         query = """
             insert into kommentar(kommentar_id, kommentar_innhold,brukernavn,innlegg_id )
-        values(%s, %s, %s, %s) 
+        values((select innlegg_id from innlegg where innlegg_id = %s) %s, %s, %s, %s) 
         """
-
-        db.cursor.execute(
-            query, (self.id, self.inhold, self.brukernavn, self.innlegg_id)
-        )
+        db.cursor.execute(query, (innlegg_id, self.id, self.inhold, self.brukernavn, self.innlegg_id))
         db.connection.commit()
         return self.get_post(db.cursor.lastrowid)
 
     def delete_kommentar(self):
-        query = """
-        delete *
-        from kommentar where
-        id = %s
-        """
+        query = """ 
+        delete from kommentar 
+        where id = %s"""
         db.cursor.execute(query, (self.id,))
         db.connection.commit()
