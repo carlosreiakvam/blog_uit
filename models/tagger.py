495d65fef19e7fb219879d57561afc92a86256a2
Jan Erik Bisseth <jbi017@uit.no>
2021-04-14 18:06:11 +0200

    Metode for å hente tagger for et innlegg

---
+++ models/tagger.py
@@ -0,0 +1,25 @@
+from werkzeug.exceptions import abort
+
+from extensions import db
+
+
+class Tagger():
+    def __init__(self,
+                 tagnavn: str = None,
+                 innleggid: int = None):
+        self.tagnavn = tagnavn
+        self.innleggid = innleggid
+
+def get_tags(innlegg_id) -> "Tagger":
+    query = """
+    select tag_navn
+    from tagger
+    where innlegg_innlegg_id = %s
+    """
+    db.cursor.execute(query, (innlegg_id,))
+    result = Tagger(*db.cursor.fetchall())
+    if result.tagnavn:
+        return result
+    else:
+        abort(404)
+
