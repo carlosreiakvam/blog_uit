ff184c4059446c51b718099b679315c076edc19d
carlosreiakvam <cre032@uit,no>
2021-04-15 16:10:01 +0200

    Lar insert_test_data() opprette brukere ved å kjøre opprett_brukere() fra brukere.py

--- database/db_setup/insert_data.py
+++ database/db_setup/insert_data.py
@@ -1,16 +1,5 @@
-from extensions import db
-from mysql.connector import errorcode, Error
-from flask import current_app
-
-TEST_DATA = {}
-
-TEST_DATA["blog"] = """ """
-TEST_DATA["brukere"] = """ """
-TEST_DATA["innlegg"] = """ """
-TEST_DATA["kommentarer"] = """ """
-TEST_DATA["tagger"] = """ """
+from database.db_setup.eksempelverdier import brukere
 
 
-def create_test_data():
-    cursor = db.connection.cursor()
-    cursor.close
+def insert_test_data():
+    brukere.opprett_brukere()
