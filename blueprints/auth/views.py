fdea84593c28868c73ca59d143da3d9d0ec53684
Hanstein Rommerud <hro047@post.uit.no>
2021-04-20 10:01:49 +0200

    Opprettet login.html og oppdaterte i blueprint views.py login.html er rett og slett den samme som i eksempelet vi fikk (login1.html). Det inneholder en del ting jeg tror ikke skal være der (som hemmelig side, etc).
    Er ekstremt usikker på akkurat hva som skulle gjøres - selve login-formen er vel der, og metoden som går til router.loginpage. Men ble veldig usikker på hvor å gå hen herfra. Har kopiert inn (og redigert litt på) noe fra eksempel i session5.py, men alt er ikke fiksa så det skal fungere som vårt. Altså veldig usikker på hvor mye som skulle gjøres etter dette, eller autentiseringen var en egen del utenom dette.

--- blueprints/auth/views.py
+++ blueprints/auth/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from models.bruker import Bruker
 
 router = Blueprint('auth', __name__, url_prefix="/auth")
 
@@ -6,3 +7,26 @@ router = Blueprint('auth', __name__, url_prefix="/auth")
 @router.route("/")
 def example():
     return "hello from auth"
+
+@router.route('/loginpage')
+# @login_required
+def login_page() -> 'html':
+    return render_template('login.html')
+
+
+'''
+@router.route('/login', methods=["GET", "POST"])
+def login() -> 'html':
+    if request.method == "POST":
+
+        username = request.form['username']
+        password = request.form['password']
+        with Bruker() as db:
+            user = User(*db.get_bruker(username))
+            if user.check_password(password):
+                user.is_authenticated = True
+                login_user(user)
+                session['user'] = user.__dict__
+
+        return redirect('/')
+'''
