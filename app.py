3223a534c191cc081fc0e9864c5f9d9e09e82805
Thomas Ytterdal <thomas.ytterdal@cern.ch>
2021-04-24 19:41:20 +0200

    Registrert blueprint og lagt til ckeditor extension og csrf for filopplasting

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template
 from blueprints.auth import router as auth_blueprint
 from blueprints.hovedside import router as hovedside_blueprint
 from blueprints.blog import router as blog_blueprint
+from blueprints.vedlegg import router as vedlegg_blueprint
 from config import config
-from extensions import db, login_manager
+from extensions import db, login_manager, ck, csrf
 from models.bruker import Bruker
 
 
@@ -14,6 +15,8 @@ def create_app(config_name="default"):
 
     db.init_app(app)
     login_manager.init_app(app)
+    ck.init_app(app)
+    csrf.init_app(app)
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -22,6 +25,7 @@ def create_app(config_name="default"):
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(hovedside_blueprint)
     app.register_blueprint(blog_blueprint)
+    app.register_blueprint(vedlegg_blueprint)
 
     return app
 
--- extensions.py
+++ extensions.py
@@ -1,6 +1,9 @@
 from flask_login import LoginManager
-
+from flask_ckeditor import CKEditor
 from database import MySQL
+from flask_wtf.csrf import CSRFProtect
 
 db = MySQL()
 login_manager = LoginManager()
+ck = CKEditor()
+csrf = CSRFProtect()
