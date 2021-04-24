5ee869b32073b9da914b71186fdf0da16e933821
Jan Erik Bisseth <jbi017@uit.no>
2021-04-24 18:46:44 +0200

    Formatering av kode

--- blueprints/hovedside/views.py
+++ blueprints/hovedside/views.py
@@ -1,6 +1,6 @@
-
 from flask import Blueprint, render_template
 from models.innlegg import Innlegg
+
 router = Blueprint('hovedside', __name__)
 
 
@@ -9,5 +9,4 @@ def index():
     new10 = Innlegg.get_ten_newest()
     if new10:
         return render_template('index.html',
-                           innlegg=new10)
-
+                               innlegg=new10)
