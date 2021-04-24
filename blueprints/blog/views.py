6b2342eb73f8f978d944bd48170dd0a02170b8fd
Thomas Ytterdal <thomas.ytterdal@cern.ch>
2021-04-24 20:09:00 +0200

    Kreve at brukeren er logget inn og eier bloggen for å kunne legge inn nytt innlegg.

--- blueprints/blog/views.py
+++ blueprints/blog/views.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, redirect, render_template, url_for, flash
 
 from blueprints.blog.forms import InnleggForm
+from models.blog import Blog
 from models.innlegg import Innlegg
+from flask_login import current_user, login_required
 
 router = Blueprint('blog', __name__, url_prefix="/blog")
 
@@ -12,8 +14,13 @@ def example():
 
 
 @router.route("/<blog_prefix>/new", methods=["GET", "POST"])
+@login_required
 def nytt_innlegg(blog_prefix: str):
     form = InnleggForm()
+    blog = Blog.get_one(blog_prefix)
+    if blog.blog_bruker_navn != current_user.brukernavn:
+        flash("Du må være eieren av en blog for å legge inn ett nytt innlegg!")
+        return redirect(url_for("hovedside.example"))
 
     if form.validate_on_submit():
         innlegg = Innlegg(innlegg_tittel=form.tittel.data, innlegg_innhold=form.innhold.data, blog_prefix=blog_prefix)
