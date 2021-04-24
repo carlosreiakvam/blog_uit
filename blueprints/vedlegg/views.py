1d6ce527f52ba65c9dd12f47225cdc2961e0460e
Thomas Ytterdal <thomas.ytterdal@cern.ch>
2021-04-24 19:40:03 +0200

    Legge til vedlegg blurprint med funksjoner for opplasting av filer

---
+++ blueprints/vedlegg/views.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, current_app, request, send_from_directory
+from flask_ckeditor import upload_fail, upload_success, url_for
+from models.vedlegg import Vedlegg
+import os
+import uuid
+
+router = Blueprint('vedlegg', __name__, url_prefix="/vedlegg")
+
+
+@router.route('/upload', methods=['POST'])
+def upload():
+    upload_dir = current_app.config.get("UPLOAD_DIR")
+    f = request.files.get('upload')
+    filename, extension = os.path.splitext(f.filename)
+    if extension not in ['.jpg', '.gif', '.png', '.jpeg']:
+        return upload_fail(message='Image only!')
+    file_uuid = uuid.uuid4().hex
+    vedlegg = Vedlegg(vedlegg_id=file_uuid, vedlegg_navn=f.filename)
+    vedlegg.insert()
+    f.save(upload_dir / file_uuid)
+    url = url_for('vedlegg.uploaded_files', filename=file_uuid)
+    return upload_success(url=url)
+
+
+@router.route('/<file_uuid>')
+def uploaded_files(file_uuid):
+    upload_dir = current_app.config.get("UPLOAD_DIR")
+    vedlegg = Vedlegg.get_by_id(file_uuid)
+    return send_from_directory(upload_dir, file_uuid, attachment_filename=vedlegg.vedlegg_navn)
