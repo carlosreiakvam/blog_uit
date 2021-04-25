7b274a2211ead457a55ef700190c0cd62a423038
Thomas Ytterdal <thomas.ytterdal@cern.ch>
2021-04-25 13:12:12 +0200

    Vis liste over tilgjengelige tagger.

--- blueprints/blog/views.py
+++ blueprints/blog/views.py
@@ -3,6 +3,7 @@ from flask import Blueprint, redirect, render_template, url_for, flash
 from blueprints.blog.forms import InnleggForm
 from models.blog import Blog
 from models.innlegg import Innlegg
+from models.tagger import Tagger
 from flask_login import current_user, login_required
 
 router = Blueprint('blog', __name__, url_prefix="/blog")
@@ -22,11 +23,18 @@ def nytt_innlegg(blog_prefix: str):
         flash("Du må være eieren av en blog for å legge inn ett nytt innlegg!")
         return redirect(url_for("hovedside.example"))
 
+    available_tags = Tagger.get_all_available_tags()
+
     if form.validate_on_submit():
         innlegg = Innlegg(innlegg_tittel=form.tittel.data, innlegg_innhold=form.innhold.data, blog_prefix=blog_prefix)
-        innlegg.insert()
+        innlegg = innlegg.insert()
+        print(form.tagger.data)
+        for tag in form.tagger.data:
+            print(tag)
+            tag = Tagger(tagnavn=tag, innleggid=innlegg.innlegg_id)
+            tag.add_tag()
         return redirect(url_for('hovedside.example'))
     for fieldName, error_messages in form.errors.items():
         for error_message in error_messages:
             flash(f"{fieldName}: {error_message}")
-    return render_template("nytt_innlegg.html", form=form, blog_prefix=blog_prefix)
+    return render_template("nytt_innlegg.html", form=form, blog_prefix=blog_prefix, available_tags=available_tags)
