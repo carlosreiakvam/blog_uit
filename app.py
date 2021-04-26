1dbb7d8a57aba10037299107fbc7396cacd1d5e4
Hanstein Rommerud <hro047@post.uit.no>
2021-04-26 22:07:48 +0200

    forsøk 2, funker ikke mye bedre.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 
-from blueprints.error import router as error_blueprint
 from blueprints.auth import router as auth_blueprint
 from blueprints.hovedside import router as hovedside_blueprint
 from blueprints.blog import router as blog_blueprint
@@ -23,9 +22,13 @@ def create_app(config_name="default"):
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(hovedside_blueprint)
     app.register_blueprint(blog_blueprint)
-    app.register_blueprint(error_blueprint)
+
+    app.register_error_handler(404, page_not_found)
 
     return app
+
+def page_not_found(e):
+  return render_template('404.html'), 404
 
 
 
--- blueprints/error/__init__.py
+++
@@ -1,1 +0,0 @@
-from blueprints.error.views import router
