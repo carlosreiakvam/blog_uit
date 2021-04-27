4a6cef63375712b40c3779b470658f8eaea4c152
Thomas Ytterdal <thomas.ytterdal@cern.ch>
2021-04-27 21:21:52 +0200

    Rydde opp imports

--- blueprints/auth/views.py
+++ blueprints/auth/views.py
@@ -1,9 +1,10 @@
-import flask
-from flask import Blueprint, render_template, abort, flash, url_for, redirect, request
+from urllib.parse import urljoin, urlparse
+
+from flask import Blueprint, abort, flash, redirect, render_template, request, url_for
 from flask_login import login_user
-from urllib.parse import urlparse, urljoin
-from models.bruker import Bruker
+
 from blueprints.auth.forms import LoginForm
+from models.bruker import Bruker
 
 router = Blueprint('auth', __name__, url_prefix="/auth")
 
@@ -15,7 +16,6 @@ def example():
 
 @router.route('/login', methods=['GET', 'POST'])
 def login():
-
     form = LoginForm()
     if form.validate_on_submit():
 
@@ -30,10 +30,11 @@ def login():
 
         next = request.args.get('next')
         if not is_safe_url(next):
-            return flask.abort(400)
+            return abort(400)
 
         return redirect(next or url_for("hovedside.index"))
     return render_template('login.html', form=form)
+
 
 def is_safe_url(target):
     ref_url = urlparse(request.host_url)
