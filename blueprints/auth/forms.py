d80c9918b73627c98410556fb0bd17286a7c64cf
carlosreiakvam <cre032@uit,no>
2021-04-28 11:31:16 +0200

    Fikset forms.py etter kommentarer.
    - Fjernet unødvendige imports
    - Lag til max i Length for brukernavn
    - Rensket opp linjeskift

--- blueprints/auth/forms.py
+++ blueprints/auth/forms.py
@@ -1,7 +1,7 @@
-import wtforms
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username')
@@ -10,17 +10,31 @@ class LoginForm(FlaskForm):
 
 
 class RegisterForm(FlaskForm):
-    brukernavn = StringField(label=('Brukernavn'),
-                             validators=[DataRequired(),
-                                         Length(min=5, message='Brukernavnet må være mellom 5 og 24 tegn')])
-    etternavn = StringField(label=('Etternavn'), validators=[DataRequired()])
-    fornavn = StringField(label=('Fornavn'), validators=[DataRequired()])
-    epost = StringField(label=('Epost'),
-                        validators=[DataRequired(), Email(message="Vennligst oppgi en gyldig epostadresse")])
-    passord = PasswordField(label=('Passord'),
-                            validators=[DataRequired(),
-                                        Length(min=8, message='Passordet må være minst 8 tegn langt')])
-    confirm = PasswordField(label=('Gjenta passord'), validators=[DataRequired(message='*Required'),
-                                                                  EqualTo('passord',
-                                                                          message='Passordene må være like')])
+    brukernavn = StringField(
+        label=('Brukernavn'),
+        validators=[DataRequired(),
+                    Length(min=5,
+                           max=24,
+                           message='Brukernavnet må være mellom 5 og 24 tegn')])
+    etternavn = StringField(
+        label=('Etternavn'),
+        validators=[DataRequired()])
+    fornavn = StringField(
+        label=('Fornavn'),
+        validators=[DataRequired()])
+    epost = StringField(
+        label=('Epost'),
+        validators=[
+            DataRequired(),
+            Email(message="Vennligst oppgi en gyldig epostadresse")])
+    passord = PasswordField(
+        label=('Passord'),
+        validators=[DataRequired(),
+                    Length(min=8,
+                           message='Passordet må være minst 8 tegn langt')])
+    confirm = PasswordField(
+        label=('Gjenta passord'),
+        validators=[DataRequired(message='*Required'),
+                    EqualTo('passord',
+                            message='Passordene må være like')])
     submit = SubmitField(label=('Registrer'))
