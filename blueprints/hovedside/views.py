e31c164d2bb4d9996dc83d07677f567a24d2b8e3
Thomas Ytterdal <thomas.ytterdal@cern.ch>
2021-05-02 18:21:08 +0200

    Lagt til melding som vises om det ikke er noen innlegg.

--- blueprints/hovedside/views.py
+++ blueprints/hovedside/views.py
@@ -10,20 +10,21 @@ def index():
     new10 = Innlegg.get_ten_newest()
     if new10:
         return render_template('index.html',
-                               innlegg=new10)
+                               innlegg=new10,
+                               no_result_message="Ingen innlegg så langt...")
 
 
 @router.route("/tag/<tag_navn>")
 def tag(tag_navn: str):
     postswithtag = Innlegg.get_with_tag(tag_navn)
-    if postswithtag and len(postswithtag) > 0:
-        return render_template('index.html',
-                               innlegg=postswithtag)
-    return abort(404)
+    return render_template('index.html',
+                           innlegg=postswithtag,
+                           no_result_message="Denne taggen har ingen innlegg...")
 
 
 @router.route("/search", methods=["GET", "POST"])
 def search():
     search_string = request.form.get("search-string", "")
     search_result = Innlegg.search(search_string)
-    return render_template("index.html", innlegg=search_result)
+    return render_template("index.html", innlegg=search_result,
+                           no_result_message="Søket ga desverre ingen resultater...")
